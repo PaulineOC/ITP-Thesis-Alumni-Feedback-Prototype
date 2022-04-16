bff6416a4dbf3bfaa9f7c7924f3a139f9a3b2584
PaulineOC <poc215@nyu.edu>
2022-04-16 00:03:39 -0400

    Room Builder - Test No Build in path

--- src/pages/RoomBuilder/index.tsx
+++ src/pages/RoomBuilder/index.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import Unity, { UnityContext } from 'react-unity-webgl';
 
 const unityContext = new UnityContext({
-    loaderUrl: "build/RoomSize-v2-UI_Button_Test.loader.js",
-    dataUrl: "build/RoomSize-v2-UI_Button_Test.data",
-    frameworkUrl: "build/RoomSize-v2-UI_Button_Test.framework.js",
-    codeUrl: "build/RoomSize-v2-UI_Button_Test.wasm",
+    loaderUrl: "RoomSize-v2-UI_Button_Test.loader.js",
+    dataUrl: "RoomSize-v2-UI_Button_Test.data",
+    frameworkUrl: "RoomSize-v2-UI_Button_Test.framework.js",
+    codeUrl: "RoomSize-v2-UI_Button_Test.wasm",
 });
 
 const RoomBuilder  = () => {
