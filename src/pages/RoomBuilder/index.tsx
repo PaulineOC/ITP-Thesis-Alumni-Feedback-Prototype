47d50a9b69057d48d101e3325e9284f32badce3d
PaulineOC <poc215@nyu.edu>
2022-04-19 02:10:19 -0400

    v3: pushing actual file changes?

--- src/pages/RoomBuilder/index.tsx
+++ src/pages/RoomBuilder/index.tsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useEffect,useState } from 'react';
 import Unity, { UnityContext } from 'react-unity-webgl';
 
 const unityContext = new UnityContext({
-    loaderUrl: "RoomSize-v2-UI_Button_Test.loader.js",
-    dataUrl: "RoomSize-v2-UI_Button_Test.data",
-    frameworkUrl: "RoomSize-v2-UI_Button_Test.framework.js",
-    codeUrl: "RoomSize-v2-UI_Button_Test.wasm",
+    loaderUrl: "v3.loader.js",
+    dataUrl: "v3.data",
+    frameworkUrl: "v3.framework.js",
+    codeUrl: "v3.wasm",
 });
 
 const RoomBuilder  = () => {
+
+    useEffect(function () {
+        unityContext.on("SendScreenshot", (imgData)=>{
+            console.log("Send Screenshot fired: ", imgData);
+        });
+    }, []);
+
+    useEffect(function () {
+        unityContext.on("debug", function (message) {
+            console.log(message);
+        });
+    }, []);
+
+
+
     return (
         <div>
             <p>Room Builder:</p>
